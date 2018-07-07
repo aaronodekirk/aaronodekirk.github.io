110b352e3f27110598468ea7f21f6ce7b9932435
aaronodekirk <35749948+aaronodekirk@users.noreply.github.com>
2018-07-07 09:15:37 -0500

    Add files via upload

---
+++ islt_7370/Odekirk-Site-Redesign-Project1/helper.js
@@ -0,0 +1,13 @@
+    function menu() {
+      var menu = document.getElementById('menu');
+      var display = menu.style.display;
+      if(display=="none")
+      {
+        menu.style.display="block";
+      }
+      else
+      {
+        menu.style.display="none";
+      }
+
+    }
